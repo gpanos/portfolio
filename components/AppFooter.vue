e69c56a97517eb7bb1dd1318a840ed59e8af2bf3
gpanos <karapanos.dimitris@gmail.com>
2021-12-03 10:04:49 +0100

    wip

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -2,12 +2,12 @@
   <footer>
     <div class="bg-white dark:bg-black max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
       <div class="flex justify-center space-x-6 md:order-2">
-        <a href="https://twitter.com/gpanos00" class="text-gray-400 hover:text-gray-500">
-          <span class="sr-only">Twitter</span>
-          <IconsGitHub class="h-6 w-6" />
-        </a>
         <a href="https://github.com/gpanos" class="text-gray-400 hover:text-gray-500">
           <span class="sr-only">GitHub</span>
+          <IconsGitHub class="h-6 w-6" />
+        </a>
+        <a href="https://twitter.com/gpanos00" class="text-gray-400 hover:text-gray-500">
+          <span class="sr-only">Twitter</span>
           <IconsTwitter class="h-6 w-6" />
         </a>
       </div>
