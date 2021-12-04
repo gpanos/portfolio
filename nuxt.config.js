d37cba7f5c407282794e83c4258aa4427bd0c977
gpanos <karapanos.dimitris@gmail.com>
2021-12-04 20:47:46 +0100

    enable ssr

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
-  ssr: false,
+  ssr: true,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
