6ca93f63d0e92bde7defec39984f4fc71d4db3ae
gpanos <karapanos.dimitris@gmail.com>
2021-12-04 20:58:29 +0100

    fix icon switch on reload

--- components/ToggleColorModeButton.vue
+++ components/ToggleColorModeButton.vue
@@ -1,8 +1,10 @@
 <template>
   <button type="button" class="inline-flex items-center justify-center text-gray-400 hover:text-gray-500" @click="toggle">
     <span class="sr-only">Toggle color mode</span>
-    <IconsSun v-if="! isDark" class="w-6 h-6 block" />
-    <IconsMoon v-else class="w-6 h-6 block" />
+    <client-only>
+      <IconsSun v-if="! isDark" class="w-6 h-6 block" />
+      <IconsMoon v-else class="w-6 h-6 block" />
+    </client-only>
   </button>
 </template>
 
